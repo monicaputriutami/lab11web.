d7d989cc481ead9d8b83582537a82195ae4d87c1
monicaputriutami <81574673+monicaputriutami@users.noreply.github.com>
2021-06-21 22:26:54 +0700

    Add files via upload

---
+++ page.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        return view('about', [
+            'title' => 'Halaman About',
+            'content' => 'Ini adalah halaman abaut yang menjelaskan tentang isi
+            halaman ini.'
+            ]);
+    }
+
+   
+
+    public function contact()
+    {
+        return view('contact', [
+            'title' => 'Halaman contact',
+            'content' => 'Ini adalah halaman abaut yang menjelaskan tentang isi
+            halaman ini.'
+            ]);
+    }
+
+    public function faqs()
+    {
+        echo "Ini halaman FAQ";
+    }
+    public function tos()
+    {
+        echo "Ini halaman Term of Service";
+    }
+}
